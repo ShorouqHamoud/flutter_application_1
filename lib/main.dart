5182a1305ba1ea38a1e914bd66fb9fda579069cd
ShorouqHamoud <mohammedhamoud640@gmail.com>
2025-11-28 19:41:40 -0800

    fff

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First App',
+      home: HomePage(),
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+
+      darkTheme: ThemeData(),
+      themeMode: ThemeMode.system,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> items = [
+    'widget 1',
+    'widget 2',
+    'widget 3',
+    'widget 4',
+    'widget 5',
+    'widget 6',
+    'widget 7',
+    'widget 8',
+    'widget 9',
+    'widget 10',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter App")),
+
+      body: Wrap(
+        spacing: 10,
+        runSpacing: 20,
+        direction: Axis.horizontal,
+        alignment: WrapAlignment.start,
+
+        textDirection: TextDirection.ltr,
+        verticalDirection: VerticalDirection.down,
+
+        children: items.map((item) {
+          return Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+            alignment: Alignment.center,
+            child: Text(
+              item,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          );
+        }).toList(),
+      ),
+
+      endDrawer: Drawer(),
+      drawer: Drawer(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.only(
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        width: 290,
+        backgroundColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/bg.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              onDetailsPressed: () {
+                Navigator.pop(context);
+              },
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/logo.png'),
+              ),
+              currentAccountPictureSize: Size.square(70),
+
+              arrowColor: Colors.red,
+              accountName: Text('User Name'),
+              accountEmail: Text('user.email@gmail.com'),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+              ],
+            ),
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/bg.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.amber,
+                    backgroundImage: AssetImage('assets/images/logo.png'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'User Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+                  Text(
+                    'user.email@gmail.com',
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ],
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorite'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Setttings'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
